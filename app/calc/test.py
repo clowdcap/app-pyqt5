0659a38fc91cfd04831332748e987e602342df95
José Marinho <68341332+clowdcap@users.noreply.github.com>
2021-10-18 11:24:10 -0300

    Update Calculo
    Adicionado funcionalidade de gerar resultado e salvar o mesmo num arquivo excel

---
+++ app/calc/test.py
@@ -0,0 +1,104 @@
+## TESTE A APLICAÇÃO AQUI
+
+from openpyxl import Workbook
+import datetime
+
+
+
+# Setup inicial
+# __init__
+arquivo_excel = Workbook()
+planilha1 = arquivo_excel.active
+
+# Colunas
+planilha1['A2'] = 'Item'
+planilha1['B2'] = 'Descrição'
+planilha1['C2'] = 'Dado'
+planilha1['A1'] = 'Registro:'
+planilha1['B1'] = datetime.datetime.now()
+
+
+area_do_terreno = 1000
+area_anteriormente_construido = 0
+area_computavel_subsolo = 0
+area_nao_computavel_subsolo = 0
+area_computavel_terreo = 500
+area_nao_computavel_terreo = 0
+area_computavel_superior = 0
+area_nao_computavel_superior = 0
+area_nao_computavel_a_construir_atico = 0
+area_livre = 0
+numero_de_pavimento = 0
+altura_total = 0
+area_de_cobertura = 0
+
+# Gerando calculos
+projecao_edificacao = area_anteriormente_construido + area_computavel_terreo + area_nao_computavel_terreo
+taxa_ocupacao = (projecao_edificacao / area_do_terreno) * 100 
+taxa_permeabilidade = (area_livre / area_do_terreno) * 100
+area_total_contruir_subsolo = area_computavel_subsolo + area_nao_computavel_subsolo
+area_total_contruir_terreo = area_computavel_terreo + area_nao_computavel_terreo
+area_total_contruir_superior = area_computavel_superior + area_nao_computavel_superior
+area_total_contruir_computavel = area_computavel_subsolo + area_computavel_terreo + area_computavel_superior
+area_total_contruir_nao_computavel = area_nao_computavel_subsolo + area_nao_computavel_terreo + area_nao_computavel_superior
+coeficiente_aproveitamento = ((area_total_contruir_computavel + area_anteriormente_construido)/area_do_terreno)
+area_total_construida_liberada = area_total_contruir_computavel + area_total_contruir_nao_computavel
+
+
+dicionario_com_resultado = {
+    'Area do terreno': area_do_terreno,
+    'area_anteriormente_construido' : area_anteriormente_construido,
+    'area_computavel_subsolo': area_computavel_subsolo,
+    'area_nao_computavel_subsolo': area_nao_computavel_subsolo,
+    'area_computavel_terreo': area_computavel_terreo,
+    'area_nao_computavel_terreo': area_nao_computavel_terreo,
+    'area_computavel_superior': area_computavel_superior,
+    'area_nao_computavel_superior': area_nao_computavel_superior,
+    'area_nao_computavel_a_construir_atico': area_nao_computavel_a_construir_atico,
+    'area_livre': area_livre,
+    'numero_de_pavimento': numero_de_pavimento,
+    'altura_total': altura_total,
+    'projecao_edificacao': projecao_edificacao,
+    'taxa_ocupacao': taxa_ocupacao,
+    'taxa_permeabilidade': taxa_permeabilidade,
+    'area_total_contruir_subsolo': area_total_contruir_subsolo,
+    'area_total_contruir_terreo': area_total_contruir_terreo,
+    'area_total_contruir_superior': area_total_contruir_superior,
+    'area_total_contruir_computavel': area_total_contruir_computavel,
+    'area_total_contruir_nao_computavel': area_total_contruir_nao_computavel,
+    'coeficiente_aproveitamento': coeficiente_aproveitamento,
+    'area_de_cobertura': area_de_cobertura,
+    'area_total_construida_liberada': area_total_construida_liberada
+}
+
+for item, descricao in enumerate(dicionario_com_resultado):
+    linha = (item+1, descricao, dicionario_com_resultado[descricao])
+    planilha1.append(linha)
+
+planilha1['D3'] = 'M²' # 1
+planilha1['D4'] = 'M²' # 2
+planilha1['D5'] = 'M²' # 3
+planilha1['D6'] = 'M²' # 4
+planilha1['D7'] = 'M²' # 5
+planilha1['D8'] = 'M²' # 6
+planilha1['D9'] = 'M²' # 7
+planilha1['D10'] = 'M²' # 8
+planilha1['D11'] = 'M²' # 9
+planilha1['D12'] = 'M²' # 10
+planilha1['D13'] = 'Pavimentos' # 11
+planilha1['D14'] = 'M' # 12
+planilha1['D15'] = 'M²' # 13
+planilha1['D16'] = '%' # 14
+planilha1['D17'] = '%' # 15
+planilha1['D18'] = 'M²' # 16
+planilha1['D19'] = 'M²' # 17
+planilha1['D20'] = 'M²' # 18
+planilha1['D21'] = 'M²' # 19
+planilha1['D22'] = 'M²' # 20
+planilha1['D23'] = '' # 21
+planilha1['D24'] = 'M²' # 22
+planilha1['D25'] = 'M²' # 23
+
+
+
+arquivo_excel.save("teste.xlsx")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from PyQt5 import uic, QtWidgets
-
+from openpyxl import Workbook
+import datetime
 
 class SistemaGeral:
     def __init__(self):
+        ### SETUP LAYOUT APP
         # Chamando sistema Layout
         app = QtWidgets.QApplication([])
-        
+
         # Layout's
         self.tela_login = uic.loadUi('./pyqt5-templates/login.ui')
         self.tela_geral = uic.loadUi('./pyqt5-templates/geral.ui')
@@ -20,8 +22,7 @@ class SistemaGeral:
         # Executar funções ao chamar
         ### TELA LOGIN BOTOES
         self.tela_login.btn_login.clicked.connect(self.logar_login) # botao logar
-        self.tela_login.btn_cadastrar.clicked.connect(self.cadastrar_login) # botao cadastrar
-        
+        self.tela_login.btn_cadastrar.clicked.connect(self.cadastrar_login) # botao cadastrar        
         
         ### TELA GERAL BOTOES
         self.tela_geral.btn_projetos.clicked.connect(self.projetos_geral) # botao projetos
@@ -29,7 +30,6 @@ class SistemaGeral:
         self.tela_geral.btn_calc.clicked.connect(self.calculo_geral) # botao calculo
         self.tela_geral.btn_voltar.clicked.connect(self.voltar_geral) # botao voltar
 
-        
         ### TELA PROJETOS BOTOES
         self.tela_projetos.btn_resumo.clicked.connect(self.resumo_projetos) # botao calculo
         self.tela_projetos.btn_situacao.clicked.connect(self.situacao_projetos) # botao situacao
@@ -37,22 +37,26 @@ class SistemaGeral:
         self.tela_projetos.btn_anterior.clicked.connect(self.anteriores_projetos) # botao anterior
         self.tela_projetos.btn_entrega.clicked.connect(self.entrega_projetos) # botao entrega
         self.tela_projetos.btn_voltar.clicked.connect(self.voltar_projetos) # botao voltar
-        
-        ### TELA RESUMO
+
+        ### TELA RESUMO BOTOES
         self.tela_projetos_resumo.btn_voltar.clicked.connect(self.voltar_projetos_resumo) # botao voltar
 
-        ### TELA E-MAIL
+        ### TELA E-MAIL BOTOES
         self.tela_email.btn_voltar.clicked.connect(self.voltar_email) # botao voltar
-        
-        ### TELA CALCULO
+
+        ### TELA CALCULO BOTOES
+        self.tela_calculo.btn_calcular.clicked.connect(self.calcular_calculo) # botao calcular
         self.tela_calculo.btn_voltar.clicked.connect(self.voltar_calculo) # botao voltar
+
+        ### SETUP
 
         # Mostrar layout
         self.tela_login.show()
 
         # Executar sistema
         app.exec_()
-        
+    
+
     
     ### LOGIN
     def logar_login(self):
@@ -132,6 +136,117 @@ class SistemaGeral:
 
 
     ### CALCULO
+    def calcular_calculo(self):
+        print('Calcular')
+
+        # Setup inicial
+        # __init__
+        arquivo_excel = Workbook()
+        planilha1 = arquivo_excel.active
+
+        # Colunas
+        planilha1['A2'] = 'Item'
+        planilha1['B2'] = 'Descrição'
+        planilha1['C2'] = 'Dado'
+        planilha1['D2'] = 'Unidade'
+        planilha1['A1'] = 'Registro:'
+        planilha1['B1'] = datetime.datetime.now()
+        
+        area_do_terreno = float(self.tela_calculo.input_area_do_terreno.text())
+        area_anteriormente_construido = float(self.tela_calculo.input_area_anteriormente_construido.text())
+        area_computavel_subsolo = float(self.tela_calculo.input_area_computavel_a_construir_no_subsolo.text())
+        area_nao_computavel_subsolo = float(self.tela_calculo.input_area_nao_computavel_a_construir_no_subsolo.text())
+        area_computavel_terreo = float(self.tela_calculo.input_area_computavel_a_construir_terreo.text())
+        area_nao_computavel_terreo = float(self.tela_calculo.input_area_nao_computavel_a_construir_terreo.text())
+        area_computavel_superior = float(self.tela_calculo.input_area_computavel_a_construir_pavSup.text())
+        area_nao_computavel_superior = float(self.tela_calculo.input_area_nao_computavel_a_construir_pavSup.text())
+        area_nao_computavel_a_construir_atico = float(self.tela_calculo.input_area_nao_computavel_a_construir_atico.text())
+        area_livre = float(self.tela_calculo.input_area_livre.text())
+        numero_de_pavimento = int(self.tela_calculo.input_numero_de_pavimento.text())
+        altura_total = float(self.tela_calculo.input_altura_total.text())
+        area_de_cobertura = float(self.tela_calculo.input_area_de_cobertura.text())
+        
+        # Gerando calculos
+        projecao_edificacao = area_anteriormente_construido + area_computavel_terreo + area_nao_computavel_terreo
+        taxa_ocupacao = (projecao_edificacao / area_do_terreno) * 100 
+        taxa_permeabilidade = (area_livre / area_do_terreno) * 100
+        area_total_contruir_subsolo = area_computavel_subsolo + area_nao_computavel_subsolo
+        area_total_contruir_terreo = area_computavel_terreo + area_nao_computavel_terreo
+        area_total_contruir_superior = area_computavel_superior + area_nao_computavel_superior
+        area_total_contruir_computavel = area_computavel_subsolo + area_computavel_terreo + area_computavel_superior
+        area_total_contruir_nao_computavel = area_nao_computavel_subsolo + area_nao_computavel_terreo + area_nao_computavel_superior
+        coeficiente_aproveitamento = ((area_total_contruir_computavel + area_anteriormente_construido)/area_do_terreno)
+        area_total_construida_liberada = area_total_contruir_computavel + area_total_contruir_nao_computavel
+
+        if area_computavel_subsolo == 0 and area_nao_computavel_subsolo == 0:
+            if numero_de_pavimento < 0 or numero_de_pavimento > 2:
+                print('Numero de pavimentos fora do permitido')
+
+        if area_de_cobertura < (area_de_cobertura*0.9):
+            print('Rever tamanho da cobertura')
+
+        dicionario_com_resultado = {
+            'Area do terreno': area_do_terreno,
+            'Area anteriormente construido' : area_anteriormente_construido,
+            'Area computavel subsolo': area_computavel_subsolo,
+            'Area nao computavel subsolo': area_nao_computavel_subsolo,
+            'Area computavel terreo': area_computavel_terreo,
+            'Area nao computavel terreo': area_nao_computavel_terreo,
+            'Area computavel superior': area_computavel_superior,
+            'Area nao computavel superior': area_nao_computavel_superior,
+            'Area nao computavel a construir atico': area_nao_computavel_a_construir_atico,
+            'Area livre': area_livre,
+            'Numero de pavimento': numero_de_pavimento,
+            'Altura total': altura_total,
+            'Projecao da edificacao': projecao_edificacao,
+            'Taxa de ocupacao': taxa_ocupacao,
+            'Taxa de permeabilidade': taxa_permeabilidade,
+            'Area total a contruir subsolo': area_total_contruir_subsolo,
+            'Area total a contruir terreo': area_total_contruir_terreo,
+            'Area total a contruir superior': area_total_contruir_superior,
+            'Area total a contruir computavel': area_total_contruir_computavel,
+            'Area total a contruir nao computavel': area_total_contruir_nao_computavel,
+            'Coeficiente de aproveitamento': coeficiente_aproveitamento,
+            'Area de cobertura': area_de_cobertura,
+            'Area total construida liberada': area_total_construida_liberada
+        }
+
+        for item, descricao in enumerate(dicionario_com_resultado):
+            linha = (item+1, descricao, dicionario_com_resultado[descricao])
+            planilha1.append(linha)
+        
+        # Retornando dados
+        # Colocando unidades nos resultados
+        planilha1['D3'] = 'M²' # 1
+        planilha1['D4'] = 'M²' # 2
+        planilha1['D5'] = 'M²' # 3
+        planilha1['D6'] = 'M²' # 4
+        planilha1['D7'] = 'M²' # 5
+        planilha1['D8'] = 'M²' # 6
+        planilha1['D9'] = 'M²' # 7
+        planilha1['D10'] = 'M²' # 8
+        planilha1['D11'] = 'M²' # 9
+        planilha1['D12'] = 'M²' # 10
+        planilha1['D13'] = 'Pavimentos' # 11
+        planilha1['D14'] = 'M' # 12
+        planilha1['D15'] = 'M²' # 13
+        planilha1['D16'] = '%' # 14
+        planilha1['D17'] = '%' # 15
+        planilha1['D18'] = 'M²' # 16
+        planilha1['D19'] = 'M²' # 17
+        planilha1['D20'] = 'M²' # 18
+        planilha1['D21'] = 'M²' # 19
+        planilha1['D22'] = 'M²' # 20
+        planilha1['D23'] = '' # 21
+        planilha1['D24'] = 'M²' # 22
+        planilha1['D25'] = 'M²' # 23
+        
+        try:
+            arquivo_excel.save("teste.xlsx")
+            print('Relatorio gerado com sucesso')
+        except:
+            print("Erro ao salvar o Relatório.")
+
     def voltar_calculo(self):
         print('Voltar')
         self.tela_calculo.close()
