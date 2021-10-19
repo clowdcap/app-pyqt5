07e07caf554f52356d64592cff330de2591e72db
José Marinho <clowdcap@hotmail.com>
2021-10-18 23:17:28 -0300

    Update send.py
    Corrigido bug de enviar e-mail

--- app/emaildef/send.py
+++ app/emaildef/send.py
@@ -3,6 +3,7 @@ from PyQt5 import uic
 
 class Enviar_Email:
     def __init__(self):
+        self.tela_email = uic.loadUi('./pyqt5-templates/email.ui')
         print('E-Mail')
         # criar integração com o outlook
         outlook = win32.Dispatch('outlook.application')
@@ -10,20 +11,22 @@ class Enviar_Email:
         # criar um email
         email = outlook.CreateItem(0)
         
+        
         # configurar as informações
-        email.To = ''
-        email.Subject = ''
+        email.To = 'jose.marinho56@gmail.com'
+        email.Subject = 'Teste'
         
         # variváveis
-        ### nome = str(input('Nome: '))
+        ### nome = self.tela_email.input_email.text()
         ### sobrenome = str(input('Sobrenome: '))
         # adicionando anexo
-        anexo = r'./anexos/legenda-pmcm.dwg'
+        anexo = r'E:\Python\app-pyqt5\app\emaildef\anexos\legenda-pmcm.dwg'
         
         try:
             email.Attachments.Add(anexo)
             print('Anexado documento...')
         except:
+            email.Attachments.Add(anexo)
             print('Sem Anexo...')
         
         email.HTMLBody = 'Segue em Anexo'
